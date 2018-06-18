b15e232bd0a0e3732b0d07c73e3080b6d22a45df
Vaibhav Pal <vaibhav171997@gmail.com>
2018-06-18 18:06:17 +0530

    18-June-2018

--- DS & Algos/DP/coin-change.cpp
+++ DS & Algos/DP/coin-change.cpp
@@ -1,51 +1,71 @@
-/* -------------------------------------------------
-
-Author :- Vaibhav Pal
-College :- Northern India Engineering College, New Delhi
-Coding Handle :- underdog_eagle
-Title :- Coin Change problem ----> O(n*sum)
-
-----------------------------------------------------*/
-#include <iostream>
-#include <string>
-#include <string.h>
-#include <vector>
-#include <set>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
+#define sz(o) ((int)o.size())
+#define all(o) o.begin(), o.end()
+#define rep(i, a, b) for(int i = (a); i <= (b); i++)
+#define repr(i, a, b) for(int i = (a); i >= (b); i--)
+#define INF 10000000
+
+typedef long long int ll;
+typedef vector<ll> vll;
 typedef vector<int> vi;
 
-#define FAST_IO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define INF 1000000000
-#define nl '\n'
-#define sp ' '
+const int N = 1e3+1;
+int n, coin[N], sum;
 
-int main()
-{
-    FAST_IO
-    int n, sum;
-    cin >> n;
-    int val[n+1];
-    for(int i = 1; i <= n; i++)
-        cin >> val[i];
-    cin >> sum;
+namespace recursive{
+    int dp[N];
 
-    int minCoins[sum + 1]; // DP table
-    minCoins[0] = 0;
-    for(int i = 1; i <= sum; i++)
-        minCoins[i] = INT_MAX;
+    int minCoins(int n, int sum){
+        if(sum == 0) return 0;
+        int res = INF;
+        rep(i, 1, n){
+            if(sum >= coin[i]) res = min(res, 1 + minCoins(n, sum-coin[i]));
 
-    for(int i = 1; i <= sum; i++){
-        for(int j = 1; j <= n; j++)
-            if(val[j] <= sum)
-                minCoins[i] = min(minCoins[i], minCoins[i - val[j]] + 1);
+        }
+        return dp[sum] = res;
     }
+}
 
-    cout << minCoins[sum] << nl;
+namespace iterative{
+    int dp[N];
 
+    int minCoins(){
+        rep(i, 1, sum) dp[i] = INF;
+
+        rep(i, 1, sum){
+            rep(j, 1, n){
+                if(j >= coin[i]) dp[i] = min(dp[i], 1 + dp[i-coin[j]]);
+            }
+        }
+
+        return dp[sum];
+    }
+}
+
+void solve(int testcase) {
+    cin >> n >> sum;
+    rep(i, 1, n) cin >> coin[i];
+    memset(recursive::dp, -1, sizeof(recursive::dp));
+    cout << recursive::minCoins(n, sum) << '\n';
+    cout << iterative::minCoins() << '\n';
+}
+
+int main(){
+    #ifdef VPAL
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout << setprecision(10);
+    clock_t b = clock();
+    int t = 1;
+    //cin >> t;
+    rep(tt, 1, t) solve(tt);
+    clock_t e = clock();
+    cerr << (double(e - b) / CLOCKS_PER_SEC) << " sec";
     return 0;
 }
